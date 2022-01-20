8c075c916fd054b285c10f0616338603d61a9391
kenngran <kenn.gran@gmail.com>
2022-01-20 14:51:30 +0100

    EnhetstestAdminKundeController jobbet med og fullførte (kanskje) alle _ikkeLoggetInn tester

--- src/test/java/oslomet/testing/EnhetstestAdminKundeController.java
+++ src/test/java/oslomet/testing/EnhetstestAdminKundeController.java
@@ -25,15 +25,111 @@ import static org.mockito.Mockito.when;
 public class EnhetstestAdminKundeController {
 
     @InjectMocks
-    // denne skal testes
-    private AdminKundeController aKuC;
+    private AdminKundeController adminKundeController;
 
     @Mock
-    // denne skal Mock'es
     private AdminRepository rep;
 
     @Mock
-    // denne skal Mock'es
     private Sikkerhet sjekk;
 
+
+    //-------TESTS--------//
+        // -NB! Dobbeltsjekk at ikke logget inne er gjort riktig
+
+    //---hentAlle---//
+    //DONE
+    @Test
+    public void hentAlle_ikkeLoggetInn(){
+
+        when(sjekk.loggetInn()).thenReturn(null);
+
+        List<Kunde> resultat = adminKundeController.hentAlle();
+
+        assertNull(resultat);
+    }
+
+    //DONE
+    @Test
+    public void hentAlle_loggetInn(){
+        Kunde kunde1 = new Kunde("01010110523",
+                "Lene", "Jensen", "Askerveien 22", "3270",
+                "Asker", "22224444", "HeiHei");
+        Kunde kunde2 = new Kunde("12345678901",
+                "Per", "Hansen", "Osloveien 82", "1234",
+                "Oslo", "12345678", "HeiHei");
+
+        List<Kunde> alleKunder = new ArrayList<>();
+        alleKunder.add(kunde1);
+        alleKunder.add(kunde2);
+
+        when(sjekk.loggetInn()).thenReturn("01010110523");
+        when(rep.hentAlleKunder()).thenReturn(alleKunder);
+
+        assertEquals(alleKunder, adminKundeController.hentAlle());
+    }
+
+    //---lagreKunde---//
+    //DONE
+    @Test
+    public void lagreKunde_ikkeLoggetInn(){
+        Kunde enKunde = new Kunde("01010110523",
+                "Lene", "Jensen", "Askerveien 22", "3270",
+                "Asker", "22224444", "HeiHei");
+
+        when(sjekk.loggetInn()).thenReturn(null);
+
+        String resultat = adminKundeController.lagreKunde(enKunde);
+
+        assertEquals("Ikke logget inn", resultat);
+    }
+    @Test
+    public void lagreKunde_loggetInn(){
+        Kunde enKunde = new Kunde("01010110523",
+                "Lene", "Jensen", "Askerveien 22", "3270",
+                "Asker", "22224444", "HeiHei");
+    }
+
+    //---endre---//
+    //DONE
+    @Test
+    public void endre_ikkeLoggetInn(){
+
+        Kunde enKunde = new Kunde("01010110523",
+                "Lene", "Jensen", "Askerveien 22", "3270",
+                "Asker", "22224444", "HeiHei");
+
+        when(sjekk.loggetInn()).thenReturn(null);
+
+        String resultat = adminKundeController.endre(enKunde);
+
+        assertEquals("Ikke logget inn", resultat);
+    }
+    @Test
+    public void endre_loggetInn(){
+        Kunde enKunde = new Kunde("01010110523",
+                "Lene", "Jensen", "Askerveien 22", "3270",
+                "Asker", "22224444", "HeiHei");
+    }
+
+    //---slett---//
+    //DONE
+    @Test
+    public void slett_ikkeLoggetInn(){
+        Kunde enKunde = new Kunde("01010110523",
+                "Lene", "Jensen", "Askerveien 22", "3270",
+                "Asker", "22224444", "HeiHei");
+
+        when(sjekk.loggetInn()).thenReturn(null);
+
+        String resultat = adminKundeController.slett(enKunde.getPersonnummer());
+
+        assertEquals("Ikke logget inn", resultat);
+    }
+    @Test
+    public void slett_loggetInn(){
+        Kunde enKunde = new Kunde("01010110523",
+                "Lene", "Jensen", "Askerveien 22", "3270",
+                "Asker", "22224444", "HeiHei");
+    }
 }
