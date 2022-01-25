306c33040c3384c00a9ab7805db4b850abed63fa
RunaBirkeland <runa.birkeland@gmail.com>
2022-01-25 13:10:29 +0100

    Update EnhetstestBankController.java
    Fjernet skeptisk arraylist

--- src/test/java/oslomet/testing/EnhetstestBankController.java
+++ src/test/java/oslomet/testing/EnhetstestBankController.java
@@ -117,16 +117,8 @@ public class EnhetstestBankController {
     public void hentTransaksjonerLoggetInn() {
 
         // arrange
-        List<Transaksjon> transaksjoner = new ArrayList<>();
-        Transaksjon trans1 = new Transaksjon(390, "kamel");
-        Transaksjon trans2 = new Transaksjon(870, "rev");
-        Transaksjon trans3 = new Transaksjon(120, "dromedar");
-        transaksjoner.add(trans1);
-        transaksjoner.add(trans2);
-        transaksjoner.add(trans3);
-
         Konto enKonto = new Konto("105010123456", "12345678901",
-                1000, "Lønnskonto", "NOK", transaksjoner);
+                1000, "Lønnskonto", "NOK", null);
 
         String fraDato = "kamel";
 
