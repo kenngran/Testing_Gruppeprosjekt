469f899a3728e4fecccdbaec1ac948727ce0fa14
ingridml <s308805@oslomet.no>
2022-01-25 12:08:26 +0100

    EnhetstestAdminKontoController: lagt til test på hentAlleKonti()

--- src/test/java/oslomet/testing/EnhetstestAdminKontoController.java
+++ src/test/java/oslomet/testing/EnhetstestAdminKontoController.java
@@ -1,25 +1,47 @@
 package oslomet.testing;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import oslomet.testing.API.AdminKontoController;
 import oslomet.testing.DAL.AdminRepository;
+import oslomet.testing.Models.Konto;
+import oslomet.testing.Models.Kunde;
 import oslomet.testing.Sikkerhet.Sikkerhet;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class EnhetstestAdminKontoController {
 
     @InjectMocks
-    private AdminKontoController aKoC;
+    private AdminKontoController adminKontoController;
 
     @Mock
     private AdminRepository rep;
 
     @Mock
     private Sikkerhet sjekk;
+
+    //TESTER
+
+    //WiP
+    @Test
+    public void hentAlleKonti(){
+        // arrange
+        when(sjekk.loggetInn()).thenReturn(null);
+
+        // act
+        List<Konto> resultat = adminKontoController.hentAlleKonti();
+
+        // assert
+        assertNull(resultat);
+    }
 }
+
