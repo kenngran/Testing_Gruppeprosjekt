fbbb8a1bea42775bd76af433d5e5876c59cfeed7
ingridml <s308805@oslomet.no>
2022-01-25 11:20:50 +0100

    La på masse kommentarer

--- src/test/java/oslomet/testing/EnhetstestAdminKundeController.java
+++ src/test/java/oslomet/testing/EnhetstestAdminKundeController.java
@@ -42,29 +42,34 @@ public class EnhetstestAdminKundeController {
     @Test
     public void hentAlle_ikkeLoggetInn(){
 
-        when(sjekk.loggetInn()).thenReturn(null);
+        // arrange
+        when(sjekk.loggetInn()).thenReturn(null); //Later som vi kjører metoden og tvinger frem "null" resultat
 
-        List<Kunde> resultat = adminKundeController.hentAlle();
+        // act
+        List<Kunde> resultat = adminKundeController.hentAlle(); //Bruker hentAlle metoden
 
-        assertNull(resultat);
+        // assert
+        assertNull(resultat); //Metode som sjekker at man får forventet svar - her null
     }
 
     //DONE
     @Test
     public void hentAlle_loggetInn(){
+        // arrange
         Kunde kunde1 = new Kunde("01010110523",
                 "Lene", "Jensen", "Askerveien 22", "3270",
                 "Asker", "22224444", "HeiHei");
         Kunde kunde2 = new Kunde("12345678901",
                 "Per", "Hansen", "Osloveien 82", "1234",
                 "Oslo", "12345678", "HeiHei");
+        //Vi skal teste en liste og derfor må vi opprette mer enn én person å teste på
 
         List<Kunde> alleKunder = new ArrayList<>();
         alleKunder.add(kunde1);
         alleKunder.add(kunde2);
 
-        when(sjekk.loggetInn()).thenReturn("Admin");
-        when(rep.hentAlleKunder()).thenReturn(alleKunder);
+        when(sjekk.loggetInn()).thenReturn("Admin"); //Later som vi logger inn
+        when(rep.hentAlleKunder()).thenReturn(alleKunder); //Later som listen alleKunder vi laget eksisterer i rep
 
         assertEquals(alleKunder, adminKundeController.hentAlle());
     }
@@ -75,13 +80,13 @@ public class EnhetstestAdminKundeController {
     public void lagreKunde_ikkeLoggetInn(){
         Kunde enKunde = new Kunde("01010110523",
                 "Lene", "Jensen", "Askerveien 22", "3270",
-                "Asker", "22224444", "HeiHei");
+                "Asker", "22224444", "HeiHei"); //arrange
 
-        when(sjekk.loggetInn()).thenReturn(null);
+        when(sjekk.loggetInn()).thenReturn(null); //arrange
 
-        String resultat = adminKundeController.lagreKunde(enKunde);
+        String resultat = adminKundeController.lagreKunde(enKunde); //act
 
-        assertEquals("Ikke logget inn", resultat);
+        assertEquals("Ikke logget inn", resultat); //assert - resultatet du fikk er forventet
     }
     @Test
     public void lagreKunde_loggetInn(){
